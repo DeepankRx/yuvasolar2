ebeb4d32392028da54713208155c07ffb73cbafc
DeepankRx <deepankpushpad10@gmail.com>
2021-07-16 13:22:06 +0530

    first commit

---
+++ frontend/src/utils.js
@@ -0,0 +1,48 @@
+export const prices = [
+  {
+    name: 'Any',
+    min: 0,
+    max: 0,
+  },
+  {
+    name: `Rs.0 to Rs1000`,
+    min: 0,
+    max: 1000,
+  },
+  {
+    name: `Rs.1000 to Rs.5000`,
+    min: 1000,
+    max: 5000,
+  },
+  {
+    name: `Rs.5000 to Rs.15000 `,
+    min: 5000,
+    max: 15000,
+  },
+  {
+    name: `>Rs.15000 `,
+    min: 15000,
+    max: 2000000,
+  },
+];
+export const ratings = [
+  {
+    name: '4stars & up',
+    rating: 4,
+  },
+
+  {
+    name: '3stars & up',
+    rating: 3,
+  },
+
+  {
+    name: '2stars & up',
+    rating: 2,
+  },
+
+  {
+    name: '1stars & up',
+    rating: 1,
+  },
+];
