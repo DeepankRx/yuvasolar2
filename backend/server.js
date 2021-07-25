cdc21ab1ffbdd45523ecd1ddc459373f32adb5d9
DeepankRx <deepankpushpad10@gmail.com>
2021-07-25 19:02:53 +0530

    socket

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
